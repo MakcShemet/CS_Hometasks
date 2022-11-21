9a77261db31ad49f79a66c7a85db102823a524f6
DevMasterSh <dev_master_sh@mail.ru>
2022-11-21 23:05:15 +0300

    More short solution task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,20 +1,15 @@
 ﻿Console.WriteLine("Введите три числа:");
-int max;
 int i = 0;
 int[] arr = new int[3];
+int max = arr[0];
 while( i < arr.Length) {
-    arr[i] = Convert.ToInt32(Console.ReadLine());
+    arr[i] = Convert.ToInt32(Console.ReadLine());    
+    if (arr[i] > max) {
+    max = arr[i];
     i++;
-} 
-
-if ((arr[0] > arr[1]) && (arr[0] > arr[2])) {
-    max = arr[0];
-}
-else if ((arr[1] > arr[0]) && (arr[1] > arr[2])) {
-    max = arr[1];
-}
-else {
-    max = arr[2];
-
+    }
+    else {
+        i++;
+    }
 }
 Console.WriteLine("Максимальное число " + max);
