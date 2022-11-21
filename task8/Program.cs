546fc3150dfba14ae387f4a036a47fc2b953c889
DevMasterSh <dev_master_sh@mail.ru>
2022-11-21 15:54:26 +0300

    Solution task8

---
+++ task8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите любое целое число:");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= N; i++) {
+    if ((i % 2) == 0) {
+        Console.Write(i + ", ");
+    }
+}
