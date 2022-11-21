3d0b3f2a3955dcff4d50048266b0e161e4c80b95
DevMasterSh <dev_master_sh@mail.ru>
2022-11-21 14:31:36 +0300

    Solution task2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) {
+    Console.WriteLine("Число " + a + " больше числа " + b);
+}
+else {
+    Console.WriteLine("Число " + a + " меньше числа " + b);
+}
