fd17a8d52c4ceb295b5d086503089326909e92d2
DevMasterSh <dev_master_sh@mail.ru>
2022-11-21 15:43:03 +0300

    Solution task6

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите любое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a % 2) == 0) {
+    Console.WriteLine("Число " + a + " четное");
+}
+else {
+    Console.WriteLine("Число " + a + " не четное");
+}
