777edaa1b393dd5544b1ac6106f376abb1c818f1
DevMasterSh <dev_master_sh@mail.ru>
2022-11-25 01:46:03 +0300

    Solution task10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100 && a > 999) {
+    Console.WriteLine("Число не трехзначное");
+}
+else {
+    int b = (a / 10) % 10;
+    Console.WriteLine($"Вторая цифра числа {a} это {b}");
+}
