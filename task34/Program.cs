82c457be7a81221fa3aaed4c34bfddbdb11264af
DevMasterSh <dev_master_sh@mail.ru>
2022-12-06 12:00:19 +0300

    New solution task34, task36, task38

--- task34/Program.cs
+++ task34/Program.cs
@@ -6,19 +6,18 @@
 void evCount() {
     int n = int.Parse(Console.ReadLine());
     int[] arr = new int[n];
-    string str = string.Empty;
     string num = "четных чисел";
     int count = 0;
     for(int i = 0; i < arr.Length; i++) {
         arr[i] = new Random().Next(100, 1000);        
         if(arr[i] % 2 == 0)
         count++;
-        str += $"{arr[i]},";
     }
     if(count > 1 && count < 5) 
     num = "четных числа";
     else if(count == 1)
     num = "четное число";
+    string str = string.Join(", ", arr);
     Console.WriteLine($"В массиве [{str}] {count}  {num}");
 }
 Console.WriteLine("Задайте длинну массива:");
---
+++ task36/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+void noEventIndexCount() {
+    int size = int.Parse(Console.ReadLine());
+    int[] arr = new int[size];
+    int sum = 0;
+    for(int i = 0; i < arr.Length; i++) {
+        arr[i] = new Random().Next(-100, 100);        
+            if(i % 2 != 0) 
+            sum += arr[i];        
+    }
+    string str = string.Join(", ", arr);
+    Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях в массиве [{str}] равна {sum}");
+}
+Console.WriteLine("Задайте зазмер массива:");
+noEventIndexCount();
