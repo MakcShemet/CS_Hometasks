1407297c29b44e6d07c0bcc7c3f6ea947a63f022
MakcShemet <14103518+MakcShemet@users.noreply.github.com>
2022-12-07 21:23:33 +0300

    Delete directory task10

--- Old_dz/task10_old/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Введите трехзначное число:");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a < 100 && a > 999) {
-    Console.WriteLine("Число не трехзначное");
-}
-else {
-    int b = (a / 10) % 10;
-    Console.WriteLine($"Вторая цифра числа {a} это {b}");
-}
