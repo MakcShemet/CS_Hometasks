b799b20ce9f11485aa19f508d47798d1f489963d
MakcShemet <14103518+MakcShemet@users.noreply.github.com>
2022-12-12 02:44:47 +0300

    Delete Old_dz directory

--- Old_dz/task8_old/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Введите любое целое число:");
-int N = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= N; i++) { 
-    if ((i % 2) != 1) { 
-        Console.Write(i + ", ");
-    }
-}
