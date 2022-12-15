28e41512487b3795391375094f23d84cebbe5ef8
DevMasterSh <dev_master_sh@mail.ru>
2022-12-15 03:25:44 +0300

    Solution task47. Changed autput

--- task47/Program.cs
+++ task47/Program.cs
@@ -19,7 +19,7 @@ void PrintArray() {
     double[,] array = CreateRandArray();
     for(int i = 0; i < array.GetLength(0); i++) {
         for(int j = 0; j < array.GetLength(1); j++) {
-            Console.Write($"{array[i,j]} ");
+            Console.Write($"{array[i,j]}\t");
         }
         Console.WriteLine();
     }
